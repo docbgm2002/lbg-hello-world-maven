b472abee53e66681747988317913f2cf121592b4
drbrym <bryan@1107products.com>
2024-11-04 13:46:26 +0000

    10 is not 11

--- src/test/java/com/qa/AppTest.java
+++ src/test/java/com/qa/AppTest.java
@@ -15,14 +15,14 @@ public class AppTest
     @Test
     public void shouldAnswerWithTrue()
     {
-        assertTrue( false );
+        assertTrue( true );
     }
 
     @Test
     public void test_shouldAnswerWithTen()
     {
         int answer = 10;
-        assertEquals(10, answer );
+        assertEquals(11, answer );
     }
 
     @Test
